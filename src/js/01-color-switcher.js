9c11451f01a03495f7a3f674a5688c830099398a
Stanislav Mankovskyi <odnorazka25@gmail.com>
2022-05-31 22:44:56 +0100

    change_color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timerId = null;
+
+refs.start.addEventListener('click', onPushStartColor);
+
+refs.stop.addEventListener('click', () => {
+  clearInterval(timerId);
+});
+
+function onPushStartColor() {
+  timerId = setInterval(changeColor, 1000);
+  console.log(timerId);
+}
+
+function changeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
